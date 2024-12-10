adb557b3aea0b442796a789a78076f7e0a695fa8
Darmatage <JasonWiserArt@gmail.com>
2024-12-10 17:13:42 -0500

    Ants no longer create errors, centipede knocks back player

--- cdan_fa24_action2/Assets/Scripts/Enemy_Scripts/EnemyPatrolSwarm.cs
+++ cdan_fa24_action2/Assets/Scripts/Enemy_Scripts/EnemyPatrolSwarm.cs
@@ -6,13 +6,18 @@ public class EnemyPatrolSwarm : MonoBehaviour{
 
 	public GameObject antPrefab;
 	public Transform spawnPoint;
+	public Transform[] movePoints;
+
+	//for testing:
+	public bool justOne = true; 
 
 	//timer:
 	private float theTimer = 0;
 	private float timeToSpawn = 0.8f;
 
 	void Start(){
-		antPrefab.transform.position = spawnPoint.position;
+		spawnPoint = movePoints[0];
+		//antPrefab.transform.position = spawnPoint.position;
 
 	}
 
@@ -21,13 +26,17 @@ public class EnemyPatrolSwarm : MonoBehaviour{
 		theTimer += 0.01f;
 		if (theTimer >= timeToSpawn){
 			theTimer = 0;
-			SpawnAnt();
+			//if (justOne == true){
+				SpawnAnt();
+			//	justOne = false; 
+			//}
 		}
     }
 
 	public void SpawnAnt(){
 		GameObject newAnt = Instantiate(antPrefab, spawnPoint.position, Quaternion.identity);
 		newAnt.GetComponent<NPC_PatrolSequencePoints>().isAnt = true;
+		newAnt.GetComponent<NPC_PatrolSequencePoints>().moveSpots = movePoints;
 	}
 
 }
--- cdan_fa24_action2/Assets/Scripts/Enemy_Scripts/NPC_PatrolSequencePoints.cs
+++ cdan_fa24_action2/Assets/Scripts/Enemy_Scripts/NPC_PatrolSequencePoints.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NPC_PatrolSequencePoints : MonoBehaviour {
 	private Animator anim;
+	private Rigidbody2D rb2D;
 	public float speed = 10f;
 	private float waitTime;
 	public float startWaitTime = 2f;
@@ -22,37 +23,44 @@ public class NPC_PatrolSequencePoints : MonoBehaviour {
 	private int rounds = 0; 
 	public int damage = 2;
 
+	bool attacking = false;
+	public float knockBackForce = 5f; 
+
 	void Start(){
 		waitTime = startWaitTime;
 		nextSpot = startSpot;
 		anim = gameObject.GetComponentInChildren<Animator>();
+		rb2D = gameObject.GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate(){
-		transform.position = Vector2.MoveTowards(transform.position, moveSpots[nextSpot].position, speed * Time.fixedDeltaTime);
+		if (attacking==false){
+			transform.position = Vector2.MoveTowards(transform.position, moveSpots[nextSpot].position, speed * Time.fixedDeltaTime);
+		
 
-		if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f){
-			if (waitTime <= 0){
-				if (moveForward == true){ previousSpot = nextSpot; nextSpot += 1; }
-				else if (moveForward == false){ previousSpot = nextSpot; nextSpot -= 1; }
-					waitTime = startWaitTime;
-			} else {
-				waitTime -= Time.fixedDeltaTime;
+			if (Vector2.Distance(transform.position, moveSpots[nextSpot].position) < 0.2f){
+				if (waitTime <= 0){
+					if (moveForward == true){ previousSpot = nextSpot; nextSpot += 1; }
+					else if (moveForward == false){ previousSpot = nextSpot; nextSpot -= 1; }
+						waitTime = startWaitTime;
+				} else {
+					waitTime -= Time.fixedDeltaTime;
+				}
 			}
+
+			//switch movement direction
+			if (nextSpot == 0) {moveForward = true; }
+			else if (nextSpot == (moveSpots.Length -1)) { moveForward = false; }
+
+			//turning the enemy
+			if (previousSpot < 0){ previousSpot = moveSpots.Length -1; }
+			else if (previousSpot > moveSpots.Length -1){ previousSpot = 0; }
+
+			if ((previousSpot == 0) && (faceRight)){ NPCTurn(); }
+			else if ((previousSpot == (moveSpots.Length -1)) && (!faceRight)) { NPCTurn(); }
+			// NOTE1: If faceRight does not change, try reversing !faceRight, above
+			// NOTE2: If NPC faces the wrong direction as it moves, set the sprite Scale X = -1.
 		}
-
-		//switch movement direction
-		if (nextSpot == 0) {moveForward = true; }
-		else if (nextSpot == (moveSpots.Length -1)) { moveForward = false; }
-
-		//turning the enemy
-		if (previousSpot < 0){ previousSpot = moveSpots.Length -1; }
-		else if (previousSpot > moveSpots.Length -1){ previousSpot = 0; }
-
-		if ((previousSpot == 0) && (faceRight)){ NPCTurn(); }
-		else if ((previousSpot == (moveSpots.Length -1)) && (!faceRight)) { NPCTurn(); }
-		// NOTE1: If faceRight does not change, try reversing !faceRight, above
-		// NOTE2: If NPC faces the wrong direction as it moves, set the sprite Scale X = -1.
 	}
 
 
@@ -72,14 +80,28 @@ public class NPC_PatrolSequencePoints : MonoBehaviour {
 		}
 	}
 
-//ant only?:
+//ant attacks, centipede stops moving and does knockback:
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag=="Player"){
 			anim.SetTrigger("Attack");
 			GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().playerGetHit(damage);
-			if (!isAnt){
+			if (isAnt==false){
+				//pause movement
+				attacking = true;
 				
+				//knockback:
+				other.gameObject.GetComponent<Player_EndKnockBack>().EndKnockBack();
+				//Add force to the player, pushing them back without teleporting:
+				Rigidbody2D pushRB = other.gameObject.GetComponent<Rigidbody2D>();
+				Vector2 moveDirectionPush = rb2D.transform.position - other.transform.position;
+				pushRB.AddForce(moveDirectionPush.normalized * knockBackForce * - 1f, ForceMode2D.Impulse);
 			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag=="Player"){
+			attacking = false;
 		}
 	}
 
