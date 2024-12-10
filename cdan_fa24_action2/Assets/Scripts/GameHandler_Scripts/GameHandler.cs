8a2f7a3484597c2a8b2c2fba3b75016d232400d8
Twerpily <emilyleaming8@gmail.com>
2024-12-09 23:48:55 -0500

    worked on own tilemap/implemented heart system+art(still not able to dig through tilemap on level 2)

--- cdan_fa24_action2/Assets/Scripts/GameHandler_Scripts/GameHandler.cs
+++ cdan_fa24_action2/Assets/Scripts/GameHandler_Scripts/GameHandler.cs
@@ -2,22 +2,30 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour {
 
       private GameObject player;
+
+      public static int playerHearts = 3;
+      public GameObject heart1;
+      public GameObject heart2;
+      public GameObject heart3;
+      public static float heart1fill = 1.0f;
+      public static float heart2fill = 1.0f;
+      public static float heart3fill = 1.0f;
+
       public static int playerHealth = 100;
       public int StartPlayerHealth = 100;
-      public TMP_Text healthText;
+      //public GameObject healthText;
 
       public static int gotTokens = 0;
-      public TMP_Text tokensText;
+      public GameObject tokensText;
 
       public bool isDefending = false;
 
-      //public static bool stairCaseUnlocked = false;
+      public static bool stairCaseUnlocked = false;
       //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
       private string sceneName;
@@ -30,6 +38,11 @@ public class GameHandler : MonoBehaviour {
                   playerHealth = StartPlayerHealth;
             //}
             updateStatsDisplay();
+
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
+            Debug.Log("pHealth = " + playerHealth + ". Hearts = " + playerHearts + ". h1fill = " + heart1fill + ". h2fill = " + heart2fill + ". h3fill = " + heart3fill );
       }
 
       public void playerGetTokens(int newTokens){
@@ -37,82 +50,110 @@ public class GameHandler : MonoBehaviour {
             updateStatsDisplay();
       }
 
-	public void playerGetHit(int damage){
-		if (isDefending == false){
-			playerHealth -= damage;
-			if (playerHealth >=0){
-				updateStatsDisplay();
-			}
-			if (damage > 0){
-				//play GetHit animation
-				player.GetComponent<PlayerHurt>().playerHit();       
-			}
-		}
+      public void playerGetHit(int damage){
+           if (isDefending == false){
+                  playerHealth -= damage;
+                  if (playerHealth > 0){
+                        if (playerHearts == 3){
+                              heart3fill = (float)playerHealth / StartPlayerHealth;
+                        }
+                       else if (playerHearts == 2){
+                              heart2fill = (float)playerHealth / StartPlayerHealth;
+                        }
+                       else if (playerHearts == 1){
+                              heart1fill = (float)playerHealth / StartPlayerHealth;
+                        }
+                        Debug.Log("pHealth = " + playerHealth + ". Hearts = " + playerHearts + ". h1fill = " + heart1fill + ". h2fill = " + heart2fill + ". h3fill = " + heart3fill );
+                        updateStatsDisplay();
+                  }
 
-		if (playerHealth > StartPlayerHealth){
-			playerHealth = StartPlayerHealth;
-			updateStatsDisplay();
-		}
+                  if (damage > 0){
+                        player.GetComponent<PlayerHurt>().playerHit();      //play GetHit animation
+                  }
+            }
 
-		if (playerHealth <= 0){
-			playerHealth = 0;
-			updateStatsDisplay();
-			playerDies();
-		}
+           if (playerHealth <= 0){
+                  playerHealth = 0;
+                  playerHearts -=1;
+                  updateStatsDisplay();
+                  playerHealth = StartPlayerHealth;
+            }
+
+           if (playerHearts <= 0){
+                  playerDies();
+            }
+
+           //if (playerHealth > StartPlayerHealth){
+           //       playerHealth = StartPlayerHealth;
+           //       updateStatsDisplay();
+           //}
       }
 
-	public void updateStatsDisplay(){
-		Debug.Log("I am updating stats");
-		healthText.text = "HEALTH: " + playerHealth;
-		tokensText.text = "GOLD: " + gotTokens;
-	}
+      public void updateStatsDisplay(){
+            if (playerHearts == 3){
+                  heart3.GetComponent<Image>().fillAmount = heart3fill;
+            }
+            else if (playerHearts == 2){
+                  heart3.SetActive(false);
+                  heart2.GetComponent<Image>().fillAmount = heart2fill;
+            }
+            else if (playerHearts == 1){
+                  heart2.SetActive(false);
+                  heart1.GetComponent<Image>().fillAmount = heart1fill;
+            }
+            else if (playerHearts == 0){
+                  heart1.SetActive(false);
+            }
 
-	public void playerDies(){
-		//player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
-		lastLevelDied = sceneName;       //allows replaying the Level where you died
-		StartCoroutine(DeathPause());
-	}
+            //Text healthTextTemp = healthText.GetComponent<Text>();
+            // healthTextTemp.text = "HEALTH: " + playerHealth;
 
-	IEnumerator DeathPause(){
-		//player.GetComponent<PlayerMove>().isAlive = false;
-		//player.GetComponent<PlayerJump>().isAlive = false;
-		yield return new WaitForSeconds(1.0f);
-		SceneManager.LoadScene("EndLose");
-	}
+            Text tokensTextTemp = tokensText.GetComponent<Text>();
+            tokensTextTemp.text = "GOLD: " + gotTokens;
+      }
 
-	public void StartGame() {
-		//SceneManager.LoadScene("Level1");
-		SceneManager.LoadScene("LevelComic");
-	}
+      public void playerDies(){
+            player.GetComponent<PlayerHurt>().playerDead();      //play Death animation
+            lastLevelDied = sceneName;       //allows replaying the Level where you died
+            StartCoroutine(DeathPause());
+      }
 
-	// Return to MainMenu
-	public void RestartGame() {
-		Time.timeScale = 1f;
-		GameHandler_PauseMenu.GameisPaused = false;
-		SceneManager.LoadScene("MainMenu");
-		// Reset all static variables here, for new games:
-		playerHealth = StartPlayerHealth;
-	}
+      IEnumerator DeathPause(){
+            player.GetComponent<PlayerMove>().isAlive = false;
+            player.GetComponent<PlayerJump>().isAlive = false;
+            yield return new WaitForSeconds(1.0f);
+            SceneManager.LoadScene("EndLose");
+      }
 
-	// Replay the Level where you died
-	public void RestartLevel() {
-		Time.timeScale = 1f;
-		GameHandler_PauseMenu.GameisPaused = false;
-		SceneManager.LoadScene(lastLevelDied);
-		// Reset all static variables here, for new games:
-		playerHealth = StartPlayerHealth;
-	}
+      public void StartGame() {
+            SceneManager.LoadScene("Level1");
+      }
 
-	public void QuitGame() {
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
-		#else
-		Application.Quit();
-		#endif
-	}
+      // Return to MainMenu
+      public void RestartGame() {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+                // Please also reset all static variables here, for new games!
+            playerHealth = StartPlayerHealth;
+      }
 
-	public void Credits() {
-		SceneManager.LoadScene("Credits");
-	}
+      // Replay the Level where you died
+      public void ReplayLastLevel() {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(lastLevelDied);
+             // Reset all static variables here, for new games:
+            playerHealth = StartPlayerHealth;
+      }
 
+      public void QuitGame() {
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+      }
+
+      public void Credits() {
+            SceneManager.LoadScene("Credits");
+      }
 }
